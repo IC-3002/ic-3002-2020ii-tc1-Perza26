341d6931cb375eb5ce7a1730dfff294894dd7daa
Perza26 <pjperazas@gmail.com>
2020-09-10 01:00:55 -0600

    Update euler.py

--- euler.py
+++ euler.py
@@ -1,8 +1,22 @@
 def e_cuadratica(n):
-    # Implemente esta función
-    return 0
+    r=1
+    while n>0:
+        r=r+(1/factorial(n))
+        n=n-1
+    return r
 
+def factorial(n):
+    f=1
+    while n>0:
+        f=f*n
+        n=n-1
+    return f
 
 def e_lineal(n):
-    # Implemente esta función
-    return 0
+    r=1
+    f=1
+    while n>0:
+        f=f*n
+        r=r+(1/f)
+        n=n-1
+    return r
